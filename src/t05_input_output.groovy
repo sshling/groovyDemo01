e3136632201b453380eac9afd75ac98f36c46f5d
shaoling <sshling@gmail.com>
2017-11-03 00:11:14 +0800

    xx

--- src/t05_input_output.groovy
+++ src/t05_input_output.groovy
@@ -1,1 +1,43 @@
+/*print "p1"
+print("p2")
+println "p3"
+println("p4")*/
+
+println()
+def age = 22
+//println("age=${age}") //解析变量必须双引号、三引号，单引号不解析
+
+def li = [1, 3]
+def map = ["a": 11]
+/*
+println("li:" + li)
+println("map:" + map)
+*/
+
+//5.2 format output
+def a = 10
+def b = 15
+//printf('The sum of %d and %d is %d\n', [a, b, a + b])
+
+def x = 1.234
+def y = 56.78
+//printf('%f from %f gives %f\n', [y, x, x - y])
+// %20s outputs the string right justified(右对齐) in a field of 20 characters.
+// %–20s left justifies(左对齐,默认) the output.
+
+def str="hi"
+/*
+printf("%10s\n",str)
+printf("%-10s\n",str)
+*/
+
+
+// 5.4 simple input
+/*
+print("your name:")
+def name = System.in.newReader().readLine();
+println("->"+name)
+*/
+
+//TODO more
 
---
+++ src/t06_method_01.groovy
@@ -0,0 +1,35 @@
+
+//6.1 method
+
+def methodName(){  //methodName are presented as program identifiers
+    // body
+}
+methodName()//  call or invoke  ...
+
+def hi(name , age){
+    print("p1");print(" and ")  //一行多句用 ; 分割
+    println("p3 $name age=${age}#")
+}
+//hi('tom',33);
+
+//6.3  default parameters
+def h2(age=28){
+    println("age=${age}")
+}
+/*
+h2()
+h2(23)
+*/
+
+//6.4 return value
+
+def h3(num){
+    return num*2
+}
+//println(h3(3)) //6
+
+//6.5 parameter passing ,pass by value
+
+
+
+
