f83a468158647c86c32d186fde05369e258fde60
shaoling5 <shaoling@jd.com>
2017-11-02 15:55:52 +0800

    init

---
+++ src/ch02/t01.groovy
@@ -0,0 +1,10 @@
+package ch02
+
+// OO ,object-oriented lang, everything in Groovy is an object.
+//123, is actually an object instance of the class Integer.
+
+//2.1 number
+println(123)
+println(0.25)
+
+
